128c85bfbd98ac891ed09fde081d08d83062e67d
Anthony <ajgoldenwings@gmail.com>
2016-04-01 17:49:58 -0500

    Fix Password Change, Map

--- scripts/app.js
+++ scripts/app.js
@@ -3,9 +3,7 @@
 (function(document) {
 'use strict';
 
-// Grab a reference to our auto-binding template
-// and give it some initial binding values
-// Learn more about auto-binding templates at http://goo.gl/Dx1u2g
+// Grab a reference to our auto-binding template and give it some initial binding values. Learn more about auto-binding templates at http://goo.gl/Dx1u2g
 var app = document.querySelector('#app');
 
 app.displayInstalledToast = function() {
@@ -15,8 +13,7 @@ app.displayInstalledToast = function() {
 	}
 };
 
-// Listen for template bound event to know when bindings
-// have resolved and content has been stamped to the page
+// Listen for template bound event to know when bindings have resolved and content has been stamped to the page
 app.addEventListener('dom-change', function() {
 	/*console.log('Elop Party ready.');*/
 });
